8384aed28b0d3db80430ffeeec1bf51735082705
Harold II Jamiro <99066605+IceAgeEmpire@users.noreply.github.com>
2025-04-03 12:55:36 +0800

    Initial commit

---
+++ src/main/java/net/iceageempire/vampirearise/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.iceageempire.vampirearise;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/iceageempire/vampirearise/VampireAriseDataGenerator.java
@@ -0,0 +1,11 @@
+package net.iceageempire.vampirearise;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VampireAriseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
