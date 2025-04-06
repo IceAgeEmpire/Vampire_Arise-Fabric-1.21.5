3b657652611d58b4886e0b3f42bcba66000be1d8
Harold II Jamiro <99066605+IceAgeEmpire@users.noreply.github.com>
2025-04-06 19:52:11 +0800

    Quick fix to tools

---
+++ src/main/java/net/iceageempire/vampirearise/item/custom/ModToolMaterials.java
@@ -0,0 +1,9 @@
+package net.iceageempire.vampirearise.item.custom;
+
+import net.iceageempire.vampirearise.util.ModTags;
+import net.minecraft.item.ToolMaterial;
+
+public class ModToolMaterials {
+    public static final ToolMaterial RUBY = new ToolMaterial(ModTags.Blocks.INCORRECT_FOR_RUBY_TOOL, 625, 7.0f,
+            2.0f, 22, ModTags.Items.RUBY_REPAIR);
+}
