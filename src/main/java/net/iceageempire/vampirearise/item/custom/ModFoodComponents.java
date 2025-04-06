26db6d50b66acf13839d87ad1a5115eb9636634e
Harold II Jamiro <99066605+IceAgeEmpire@users.noreply.github.com>
2025-04-06 19:53:30 +0800

    Removed recipe because data gen exist. Some Fixes

--- src/main/java/net/iceageempire/vampirearise/item/custom/ModFoodComponents.java
+++ src/main/java/net/iceageempire/vampirearise/item/custom/ModFoodComponents.java
@@ -1,8 +1,23 @@
 package net.iceageempire.vampirearise.item.custom;
 
+import net.minecraft.component.type.ConsumableComponent;
 import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
+
+import static net.minecraft.component.type.ConsumableComponents.food;
 
 public class ModFoodComponents {
     public static final FoodComponent RUBY_POTATO = new FoodComponent.Builder().nutrition(1).saturationModifier(0.3f)
             .alwaysEdible().build();
+
+    public static final ConsumableComponent RUBY_POTATO_EFFECT = food()
+            .consumeEffect(
+                    new ApplyEffectsConsumeEffect(
+                            new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 18000, 1)
+                    )
+            )
+            .build();
+
 }
